c6717381e1127c566a0f3e58ac7746cb0ce7419e
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-03 20:06:20 +0100

    remove tide init command

--- home/common.nix
+++ home/common.nix
@@ -38,9 +38,12 @@
   programs.fish = {
       enable = true;
       interactiveShellInit = ''
+        set MANPATH "usr/local/man:$MANPATH"
+        set PATH "$HOME/.local/bin:/etc/profiles/per-user/minhtung0404/bin/:$PATH"
+
         set fish_greeting # Disable greeting
 
-        tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
+        # tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
       '';
       plugins = [
         # Enable a plugin (here grc for colorized command output) from nixpkgs
@@ -79,8 +82,4 @@
     "--cmd j"
   ];
 
-
-  # home.sessionVariables = {
-  #   PATH = "/etc/profiles/per-user/${config.home.username}/bin:$PATH";
-  # };
 }
