07abd7c46f12288371979379f4642058c33bb17a
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-04 23:07:55 +0100

    refactor

--- home/common.nix
+++ home/common.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
- 
+
 {
   home.stateVersion = "24.11";
 
   programs.home-manager.enable = true;
   programs.direnv.enable = true;
- 
+
   home.packages = with pkgs; [
     aerospace
     aldente
@@ -36,53 +36,10 @@
     nixfmt-classic
   ];
 
-  programs.fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set MANPATH "usr/local/man:$MANPATH"
-        set PATH "$HOME/.local/bin:/etc/profiles/per-user/minhtung0404/bin/:$PATH"
-
-        set fish_greeting # Disable greeting
-
-        fish_vi_key_bindings
-
-        # tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
-      '';
-      plugins = [
-        # Enable a plugin (here grc for colorized command output) from nixpkgs
-        { name = "grc"; src = pkgs.fishPlugins.grc.src; }
-        {
-          name = "plugin-git";
-          src = pkgs.fishPlugins.plugin-git.src;
-        }
-        {
-            name = "tide";
-            src = pkgs.fishPlugins.tide.src;
-        }
-      ];
-      shellAliases = {
-        ls    = "eza --hyperlink --group-directories-first --icons=auto";
-        la    = "ls -a";
-        ll    = "ls -lha";
-        ld    = "ls -lhaD";
-        mkdir = "mkdir -p";
-        c     = "clear";
-        v     = "nvim";
-        vv    = "v .";
-        sv    = "sudo nvim";
-        mv    = "mv -i";
-        cp    = "cp -i";
-        ln    = "ln -i";
-        m     = "make";
-        lg    = "lazygit";
-        rgp   = "rg --fixed-strings --ignore-case";
-      };
-  };
+  imports = [ ../modules/shells/fish ];
 
   programs.zoxide.enable = true;
-  programs.zoxide.enableFishIntegration= true;
-  programs.zoxide.options = [
-    "--cmd j"
-  ];
+  programs.zoxide.enableFishIntegration = true;
+  programs.zoxide.options = [ "--cmd j" ];
 
 }
