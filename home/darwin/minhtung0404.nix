2c823706667e749d5a41d029a7d5fa2faf204b2a
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-29 14:45:04 +0100

    add veracrypt mount

--- home/darwin/minhtung0404.nix
+++ home/darwin/minhtung0404.nix
@@ -21,6 +21,20 @@ in {
     ];
   };
 
+  programs.fish.functions = {
+    drive_mount = {
+      body = ''
+        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --mount /Users/minhtung0404/Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE --password $(cat /run/secrets/veracrypt_drive) --pim 0 --keyfiles "" --protect-hidden no --slot 1 --verbose
+      '';
+    };
+
+    drive_dismount = {
+      body = ''
+        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --dismount /Users/minhtung0404/Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE
+      '';
+    };
+  };
+
   # sops.defaultSopsFile = ../../secrets/secrets.yaml;
   # sops.defaultSopsFormat = "yaml";
 
