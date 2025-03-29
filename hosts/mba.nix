4637ba1f814aaedc0ab43180268a33ad99dd4bcf
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-29 11:12:29 +0100

    add extra-nix-path

--- hosts/mba.nix
+++ hosts/mba.nix
@@ -20,6 +20,7 @@
       auto-optimise-store = true
       experimental-features = nix-command flakes
       extra-platforms = x86_64-darwin aarch64-darwin
+      extra-nix-path = nixpkgs=flake:nixpkgs
     '';
     gc = {
       automatic = true;
