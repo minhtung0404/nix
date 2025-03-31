ea1cfb7cfa3ab0c2f766bbef3c8dd619a354c8fb
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-31 21:16:32 +0200

    update kakoune

--- modules/editors/kakoune/default.nix
+++ modules/editors/kakoune/default.nix
@@ -1,11 +1,16 @@
 { config, lib, ... }:
 let cfg = config.mtn.programs.my-kakoune;
 in {
+  # imports = [ ./kak-lsp.nix ./tree-sitter.nix ];
   options.mtn.programs.my-kakoune = { enable = lib.mkEnableOption "kakoune"; };
   config = lib.mkIf cfg.enable {
     programs.kakoune = {
       enable = true;
 
+      config = {
+        indentWidth = 2;
+        ui = { assistant = "cat"; };
+      };
     };
   };
 }
