bd290ff68477ce5312f5c2ac7f8048f8eefb5723
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-31 11:16:31 +0200

    sketchybar: add sbarlua

--- modules/gui/sketchybar/default.nix
+++ modules/gui/sketchybar/default.nix
@@ -22,6 +22,31 @@ let
 
     make
   '';
+
+  # copy from https://github.com/NixOS/nixpkgs/pull/389411
+  sbarlua = pkgs.lua54Packages.buildLuaPackage {
+    name = "sbarlua";
+    pname = "sbarlua";
+    version = "0-unstable-2024-10-12";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "FelixKratz";
+      repo = "SbarLua";
+      rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
+      sha256 = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
+    };
+
+    buildInputs = [
+      pkgs.gcc
+      pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+      pkgs.readline
+    ];
+
+    installPhase = ''
+      mkdir -p $out/lib/lua/5.4/
+      cp bin/sketchybar.so $out/lib/lua/5.4
+    '';
+  };
   username = config.mtn.username;
 in {
   options.mtn.services.my-sketchybar = {
@@ -54,7 +79,7 @@ in {
             ((map (x: "${x}/bin/:") ([ cfg.package ] ++ cfg.extraPackages)) ++ [
               "/Users/${username}.nix-profile/bin:/etc/profiles/per-user/${username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin/:/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/:/sbin"
             ]);
-          LUA_CPATH = "/Users/${username}/.local/share/sketchybar_lua/?.so";
+          LUA_CPATH = "${sbarlua}/lib/lua/5.4/?.so;";
         };
         ProgramArguments = [
           "${cfg.package}/bin/sketchybar"
