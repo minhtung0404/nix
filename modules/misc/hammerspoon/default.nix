49b943c8489ffacb4a69f1b53246649e014bf78f
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-03-31 11:31:00 +0200

    hs: add EmmyLua

--- modules/misc/hammerspoon/default.nix
+++ modules/misc/hammerspoon/default.nix
@@ -1,6 +1,18 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  spoons = pkgs.fetchFromGitHub {
+    owner = "Hammerspoon";
+    repo = "Spoons";
+    rev = "95958fc6091491e8269ec2dfc6b97d4a91af9205";
+    hash = "sha256-NCKuBg7opn8BeP1FTpG0cchYdjlea6sbAaVpX6OApxg=";
+  };
+in {
   xdg.configFile.hammerspoon = {
     source = ./config;
+    recursive = true;
+  };
+  xdg.configFile."hammerspoon/Spoons/EmmyLua.spoon" = {
+    source = "${spoons}/Source/EmmyLua.spoon";
     recursive = true;
   };
   home.sessionPath = [ "/usr/bin" "/bin" "/usr/sbin" "/sbin" ];
