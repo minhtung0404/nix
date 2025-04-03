4450f1dc8c91296b848dc78d7cff5f387972caed
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-04-03 12:24:30 +0200

    move sops back to nix-darwin

--- hosts/macM1/configuration.nix
+++ hosts/macM1/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, outputs, pkgs, lib, myLib, overlays, ... }: {
+{ inputs, outputs, pkgs, lib, myLib, overlays, config, ... }: {
   imports = [
     inputs.home-manager.darwinModules.home-manager
     {
@@ -24,6 +24,7 @@
         autoMigrate = true;
       };
     }
+    inputs.sops-nix.darwinModules.sops
   ];
 
   programs.fish = {
@@ -151,6 +152,29 @@
         ipv6 = true;
       };
     };
+  };
+
+  home-manager.users.minhtung0404.programs.fish.functions = {
+    drive_mount = {
+      body = ''
+        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --mount /Users/minhtung0404/Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE --password $(cat ${config.sops.secrets.veracrypt_drive.path}) --pim 0 --keyfiles "" --protect-hidden no --slot 1 --verbose
+      '';
+    };
+
+    drive_dismount = {
+      body = ''
+        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --dismount /Users/minhtung0404Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE
+      '';
+    };
+  };
+
+  sops = {
+    age.keyFile = "/Users/minhtung0404/.config/sops/age/keys.txt";
+
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+
+    secrets.veracrypt_drive = { owner = "minhtung0404"; };
   };
 
   launchd.daemons.dnscrypt-proxy.serviceConfig.UserName = lib.mkForce "root";
--- hosts/macM1/minhtung0404.nix
+++ hosts/macM1/minhtung0404.nix
@@ -17,27 +17,4 @@ in {
 
   home.packages = with pkgs; [ texlive.combined.scheme-full sops ];
 
-  programs.fish.functions = {
-    drive_mount = {
-      body = ''
-        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --mount ${home}/Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE --password $(cat ${config.sops.secrets.veracrypt_drive.path}) --pim 0 --keyfiles "" --protect-hidden no --slot 1 --verbose
-      '';
-    };
-
-    drive_dismount = {
-      body = ''
-        /Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt --text --dismount ${home}/Library/CloudStorage/GoogleDrive-minhtung04042001@gmail.com/My\ Drive/encrypted/drive_encrypted /Volumes/DRIVE
-      '';
-    };
-  };
-
-  sops = {
-    age.keyFile = "${home}/.config/sops/age/keys.txt";
-
-    defaultSopsFile = ../../secrets/secrets.yaml;
-    defaultSopsFormat = "yaml";
-
-    secrets.veracrypt_drive = { mode = "0440"; };
-  };
-
 }
