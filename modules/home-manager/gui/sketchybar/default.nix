34c8387b891b38668238b96db964a9d57fe16647
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-04-14 19:46:49 +0200

    sketchybar: change launchd to Interactive

--- modules/home-manager/gui/sketchybar/default.nix
+++ modules/home-manager/gui/sketchybar/default.nix
@@ -1,9 +1,21 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   inherit (lib)
-    literalExpression maintainers mkEnableOption mkIf mkPackageOption mkOption
-    optionals types;
+    literalExpression
+    maintainers
+    mkEnableOption
+    mkIf
+    mkPackageOption
+    mkOption
+    optionals
+    types
+    ;
   cfg = config.mtn.services.my-sketchybar;
   sketchybarconf = pkgs.runCommandCC "sketchybar-config" { } ''
     echo Creating sketchybar config
@@ -48,7 +60,8 @@ let
     '';
   };
   username = config.mtn.username;
-in {
+in
+{
   options.mtn.services.my-sketchybar = {
     enable = mkEnableOption "my-sketchybar";
 
@@ -75,12 +88,22 @@ in {
       enable = true;
       config = {
         EnvironmentVariables = {
-          PATH = lib.concatStrings
-            ((map (x: "${x}/bin/:") ([ cfg.package ] ++ cfg.extraPackages)) ++ [
+          PATH = lib.concatStrings (
+            (map (x: "${x}/bin/:") ([ cfg.package ] ++ cfg.extraPackages))
+            ++ [
               "/Users/${username}.nix-profile/bin:/etc/profiles/per-user/${username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin/:/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/:/sbin"
-            ]);
+            ]
+          );
           LUA_CPATH = "${sbarlua}/lib/lua/5.4/?.so;";
         };
+        LimitLoadToSessionType = [
+          "Aqua"
+          "Background"
+          "LoginWindow"
+          "StandardIO"
+          "System"
+        ];
+        ProcessType = "Interactive";
         ProgramArguments = [
           "${cfg.package}/bin/sketchybar"
           "--config"
@@ -88,8 +111,8 @@ in {
         ];
         KeepAlive = true;
         RunAtLoad = true;
-        StandardOutPath = "/tmp/sbar_out_${username}.log";
-        StandardErrorPath = "/tmp/sbar_err_${username}.log";
+        StandardOutPath = "/tmp/${username}/sbar.out.log";
+        StandardErrorPath = "/tmp/${username}/sbar.err.log";
       };
     };
   };
