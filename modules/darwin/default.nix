d8505202c5415058a1f57421e8aca25b9c412c8a
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-05-26 16:47:52 +0200

    move homebrew from shared to darwin

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,36 @@
-{ ... }:
+{ lib, config, ... }:
 {
   imports = [
     ../shared
   ];
+
+  homebrew = {
+    enable = true;
+
+    brews = [ "mas" ];
+
+    casks = [
+      "scroll-reverser"
+      "hammerspoon"
+    ];
+
+    masApps = {
+      "Messenger" = 1480068668;
+      "Bitwarden" = 1352778147;
+    };
+
+    onActivation.cleanup = "zap";
+  };
+
+  launchd.daemons.dnscrypt-proxy.serviceConfig.UserName = lib.mkForce "root";
+
+  system.activationScripts = {
+    postActivation = {
+      text = lib.mkOrder 1600 ''
+        echo "Permission required ..."
+        echo "kanata: Please enable Input Mornitoring/Full Disk Access for ${config.mtn.services.my-kanata.package}/bin/kanata"
+      '';
+    };
+  };
+
 }
--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,5 @@
 {
   outputs,
-  lib,
-  config,
   ...
 }:
 {
@@ -35,38 +33,8 @@
   };
   nixpkgs.flake.setNixPath = true;
 
-  homebrew = {
-    enable = true;
-
-    brews = [ "mas" ];
-
-    casks = [
-      "scroll-reverser"
-      "hammerspoon"
-    ];
-
-    masApps = {
-      "Messenger" = 1480068668;
-      "Bitwarden" = 1352778147;
-    };
-
-    onActivation.cleanup = "zap";
-  };
-
   programs.fish = {
     enable = true;
     vendor.completions.enable = true;
   };
-
-  launchd.daemons.dnscrypt-proxy.serviceConfig.UserName = lib.mkForce "root";
-
-  system.activationScripts = {
-    postActivation = {
-      text = lib.mkOrder 1600 ''
-        echo "Permission required ..."
-        echo "kanata: Please enable Input Mornitoring/Full Disk Access for ${config.mtn.services.my-kanata.package}/bin/kanata"
-      '';
-    };
-  };
-
 }
