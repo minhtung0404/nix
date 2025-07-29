e89b3bd8ac48e81e783f3424dcaaab79e0004b89
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-07-29 09:30:31 +0200

    feat(sketchybar): use nixpkgs for sbarlua instead

--- modules/home-manager/darwin/services/sketchybar/default.nix
+++ modules/home-manager/darwin/services/sketchybar/default.nix
@@ -34,29 +34,6 @@ let
     make
   '';
 
-  # copy from https://github.com/NixOS/nixpkgs/pull/389411
-  sbarlua = pkgs.lua54Packages.buildLuaPackage {
-    name = "sbarlua";
-    pname = "sbarlua";
-    version = "0-unstable-2024-10-12";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "FelixKratz";
-      repo = "SbarLua";
-      rev = "437bd2031da38ccda75827cb7548e7baa4aa9978";
-      sha256 = "sha256-F0UfNxHM389GhiPQ6/GFbeKQq5EvpiqQdvyf7ygzkPg=";
-    };
-
-    buildInputs = [
-      pkgs.gcc
-      pkgs.readline
-    ];
-
-    installPhase = ''
-      mkdir -p $out/lib/lua/5.4/
-      cp bin/sketchybar.so $out/lib/lua/5.4
-    '';
-  };
   username = config.mtn.username;
 in
 {
@@ -92,7 +69,7 @@ in
               "/Users/${username}.nix-profile/bin:/etc/profiles/per-user/${username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin/:/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/:/sbin"
             ]
           );
-          LUA_CPATH = "${sbarlua}/lib/lua/5.4/?.so;";
+          LUA_CPATH = "${pkgs.sbarlua}/lib/lua/5.4/?.so;";
         };
         LimitLoadToSessionType = [
           "Aqua"
