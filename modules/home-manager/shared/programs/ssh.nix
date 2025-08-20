9712d1932a17cbd30a689e050364ec684a9bbe1a
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-08-21 00:09:13 +0200

    feat(ssh): fix noname config

--- modules/home-manager/shared/programs/ssh.nix
+++ modules/home-manager/shared/programs/ssh.nix
@@ -21,6 +21,15 @@
         hostname = "192.168.1.110";
         port = 2222;
         user = "mnguyen1";
+
+        localForwards = [
+          {
+            bind.address = "127.0.0.1";
+            bind.port = 9999;
+            host.port = 9999;
+            host.address = "localhost";
+          }
+        ];
       };
       "tp-*" = {
         hostname = "%h";
