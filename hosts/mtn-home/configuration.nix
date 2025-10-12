00cc558425dfed1bdc0ff6e70a4c3129bfade338
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-10-12 22:40:57 +0200

    feat: open port 80

--- hosts/mtn-home/configuration.nix
+++ hosts/mtn-home/configuration.nix
@@ -184,7 +184,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 80 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
