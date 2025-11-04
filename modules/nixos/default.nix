0f225acd9b2640339b6955a31b941f0ed215a0ad
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-11-04 12:57:24 +0100

    feat: add portal to open files

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -155,6 +155,35 @@ in
         ExecStop = "${pkgs.iproute2}/bin/ip netns del %I";
       };
     };
+    # Portals
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      xdgOpenUsePortal = true;
+      # gtk portal needed to make gtk apps happy
+      extraPortals = [
+        pkgs.kdePackages.xdg-desktop-portal-kde
+        pkgs.xdg-desktop-portal-gtk
+      ];
+
+      config.sway.default = [
+        "wlr"
+        "kde"
+        "kwallet"
+      ];
+      config.niri = {
+        default = [
+          "kde"
+          "gnome"
+          "gtk"
+        ];
+        # "org.freedesktop.impl.portal.Access" = "gtk";
+        # "org.freedesktop.impl.portal.Notification" = "gtk";
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.Secret" = "kwallet";
+        "org.freedesktop.impl.portal.FileChooser" = "kde";
+      };
+    };
     # default settings
 
     # Use the systemd-boot EFI boot loader.
