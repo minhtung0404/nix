33978190170b281f9474f3ca5fa966a29d2b8745
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-11-06 13:25:02 +0100

    feat(rclone): add reminder for Full Disk Access

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -2,6 +2,7 @@
   config,
   inputs,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -53,6 +54,7 @@
         echo "-----------------------------------------------"
         echo "Permission required ..."
         echo "kanata: Please enable Input Mornitoring/Full Disk Access for ${config.mtn.services.my-kanata.package}/bin/kanata"
+        echo "rclone: Please enable Full Disk Access for ${pkgs.rclone}/bin/rclone"
       '';
     };
   };
