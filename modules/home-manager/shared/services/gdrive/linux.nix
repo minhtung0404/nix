c294060ba461419b8b52da4f1f9474dbf3bcc25d
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-11-16 19:53:25 +0100

    feat(rclone): disable rclone bisync

--- modules/home-manager/shared/services/gdrive/linux.nix
+++ modules/home-manager/shared/services/gdrive/linux.nix
@@ -35,31 +35,31 @@ in
       };
     };
 
-    # Optional: create a systemd service to sync automatically
-    systemd.user.services.rclone-sync = {
-      Unit = {
-        Description = "Sync Google Drive using rclone";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-      Service = {
-        ExecStart = "${pkgs.rclone}/bin/rclone bisync crypt: %h/Documents/encrypted/ --resync --check-access --fast-list --drive-skip-gdocs --create-empty-src-dirs --verbose";
-        Restart = "on-failure";
-      };
-    };
+    # # Optional: create a systemd service to sync automatically
+    # systemd.user.services.rclone-sync = {
+    #   Unit = {
+    #     Description = "Sync Google Drive using rclone";
+    #   };
+    #   Install = {
+    #     WantedBy = [ "default.target" ];
+    #   };
+    #   Service = {
+    #     ExecStart = "${pkgs.rclone}/bin/rclone bisync crypt: %h/Documents/encrypted/ --resync --check-access --fast-list --drive-skip-gdocs --create-empty-src-dirs --verbose";
+    #     Restart = "on-failure";
+    #   };
+    # };
 
-    systemd.user.timers.rclone-sync = {
-      Unit = {
-        Description = "Periodic rclone sync";
-      };
-      Install = {
-        WantedBy = [ "timers.target" ];
-      };
-      Timer = {
-        OnCalendar = "*:0/10";
-        Persistent = true;
-      };
-    };
+    # systemd.user.timers.rclone-sync = {
+    #   Unit = {
+    #     Description = "Periodic rclone sync";
+    #   };
+    #   Install = {
+    #     WantedBy = [ "timers.target" ];
+    #   };
+    #   Timer = {
+    #     OnCalendar = "*:0/10";
+    #     Persistent = true;
+    #   };
+    # };
   };
 }
