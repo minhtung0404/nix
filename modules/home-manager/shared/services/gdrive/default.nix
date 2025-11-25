592494f48f409c18c367b18f8312bb1df7467c66
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2025-11-25 14:03:30 +0100

    feat(rclone): add short sync command

--- modules/home-manager/shared/services/gdrive/default.nix
+++ modules/home-manager/shared/services/gdrive/default.nix
@@ -17,5 +17,35 @@ in
   };
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [ rclone ];
+    programs.fish = {
+      functions = {
+        gsync = {
+          body = ''
+            # Ensure an argument was provided
+            if test (count $argv) -lt 1
+                echo "Usage: gsync (up|down)"
+                exit 1
+            end
+
+            set direction $argv[1]
+
+            switch $direction
+                case up
+                    echo "Syncing to drive ..."
+                    rclone sync ~/Documents/encrypted/ crypt: --progress
+
+                case down
+                    echo "Syncing from drive ..."
+                    rclone sync crypt: ~/Documents/encrypted/ --progress
+
+                case '*'
+                    echo "Unknown command: $direction"
+                    echo "Usage: gsync (up|down)"
+                    exit 1
+            end
+          '';
+        };
+      };
+    };
   };
 }
