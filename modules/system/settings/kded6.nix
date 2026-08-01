6cf06070c8bf08b066d02c5f4a54e3ddbc7625f9
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2026-08-01 12:18:04 +0200

    feat: update kded6 services

--- modules/system/settings/kded6.nix
+++ modules/system/settings/kded6.nix
@@ -9,7 +9,7 @@
 
     {
       systemd.user.services = {
-        kded6-persistent = {
+        kded6-prestart = {
           description = "KDE Daemon (persistent, prevents on-demand D-Bus re-activation under niri)";
           partOf = [ "graphical-session.target" ];
           after = [ "graphical-session.target" ];
@@ -20,12 +20,29 @@
             ExecStart = "${pkgs.kdePackages.kded}/bin/kded6";
             Restart = "on-failure";
             RestartSec = 1;
+            Slice = "session.slice";
+          };
+        };
+
+        xdg-desktop-portal-kde-prestart = {
+          description = "Pre-started KDE portal backend";
+          wantedBy = [ "graphical-session.target" ];
+          after = [
+            "graphical-session.target"
+            "kded6-prestart.service"
+          ];
+          partOf = [ "graphical-session.target" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.kdePackages.xdg-desktop-portal-kde}/libexec/xdg-desktop-portal-kde";
+            Restart = "on-failure";
+            RestartSec = "1s";
+            Slice = "session.slice";
           };
         };
 
         xdg-desktop-portal-gtk = {
           after = [
-            "kded6-persistent.service"
+            "kded6-prestart.service"
             "graphical-session.target"
           ];
         };
