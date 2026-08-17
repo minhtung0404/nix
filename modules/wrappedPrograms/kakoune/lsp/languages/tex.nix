63c55bb677f28c7be766f9a66469633f72bf6790
Minh Tung NGUYEN <minhtung04042001@gmail.com>
2026-08-17 11:27:39 +0200

    feat(kak): add tex lsp support

---
+++ modules/wrappedPrograms/kakoune/lsp/languages/tex.nix
@@ -0,0 +1,61 @@
+{
+  flake.wrappers.kakoune = { pkgs, ... }: {
+    kak-lsp.languageServers = {
+      texlab = {
+        package = pkgs.texlab;
+        filetypes = [ "latex" ];
+        settings = {
+          command = "texlab";
+          root_globs = [
+            "main.tex"
+            "all.tex"
+            "latexmkrc"
+            ".latexmkrc"
+            ".git"
+          ];
+          settings_section = "texlab";
+          settings.texlab = {
+            build.executable = "latexmk";
+            build.args = [
+              "-pdf"
+              "-shell-escape"
+              "-interaction=nonstopmode"
+              "-synctex=1"
+              "%f"
+            ];
+
+            build.forwardSearchAfter = true;
+            build.onSave = true;
+
+            forwardSearch = {
+              executable = "okular";
+              args = [
+                "--unique"
+                "file:%p#src:%l%f"
+              ];
+            };
+          };
+        };
+      };
+
+      ltex-ls = {
+        package = pkgs.ltex-ls;
+        filetypes = [
+          "latex"
+          "typst"
+        ];
+        settings = {
+          command = "ltex-ls";
+          args = [ "--log-file=/tmp" ];
+          root_globs = [
+            "main.tex"
+            "main.typ"
+            "latexmkrc"
+            ".latexmkrc"
+            ".git"
+          ];
+        };
+      };
+    };
+  };
+}
